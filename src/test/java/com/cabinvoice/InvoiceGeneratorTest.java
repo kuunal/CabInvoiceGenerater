3cb6478625e8bdd934cbd93f89c2d9f7d5f6c106
250399 <kunaldeshmukh2503@gmail.com>
2020-05-13 17:13:27 -0700

    ADDED: Getting started

---
+++ src/test/java/com/cabinvoice/InvoiceGeneratorTest.java
@@ -0,0 +1,12 @@
+package com.cabinvoice;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InvoiceGeneratorTest {
+
+
+}
